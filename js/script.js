afd49a94f371be0631948928ae53d63fcc7b6b0b
AntonioLoco <antoniolocorotondo23@gmail.com>
2022-10-12 15:37:38 +0200

    Milestone 3

--- js/script.js
+++ js/script.js
@@ -7,15 +7,17 @@ const sliderImg = [
 ]
 
 const carouselImg = document.querySelector(".carousel-img");
+const prevBtn = document.querySelector(".btn.prev");
+const nextBtn = document.querySelector(".btn.next");
 
 // Inseriamo automaticamente le immagini
 for(let i = 0; i < sliderImg.length; i++){
     // Creo la stringa da aggiungere
-    const element = 
+    const element =
     `
         <div class="item">
             <img src="${sliderImg[i]}" alt="">
-        </div>    
+        </div>
     `;
 
     // Aggiungo la stringa all HTML
@@ -28,3 +30,41 @@ console.log(itemsList);
 
 let positionSlider = 0;
 itemsList[positionSlider].classList.add("active");
+
+
+// Gestiamo l'evento click sul bottone next
+nextBtn.addEventListener("click", function(){
+
+    // Controlliamo se l'indice non supera la lunghezza della lista immagini
+    if( positionSlider < itemsList.length - 1){
+
+        // Rimuovere la classe active all'elemento corrente
+        itemsList[positionSlider].classList.remove("active");
+    
+        //Incrementare la variabile della posizione
+        positionSlider++;
+    
+        // Aggiungere la classe active al nuovo elemento
+        itemsList[positionSlider].classList.add("active");
+    }
+
+});
+
+
+// Gestiamo l'evento click sul bottone prev
+prevBtn.addEventListener("click", function(){
+
+    // Controlliamo se l'indice è maggiore di 0
+    if( positionSlider > 0){
+        // Rimuovere la classe active all'elemento corrente
+        itemsList[positionSlider].classList.remove("active");
+    
+        //Decrementare la variabile della posizione
+        positionSlider--;
+    
+        // Aggiungere la classe active al nuovo elemento
+        itemsList[positionSlider].classList.add("active");
+    }
+
+});
+
